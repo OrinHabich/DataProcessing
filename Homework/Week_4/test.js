045b73b1edd3b830bbd764da986fe314dcf00111
OrinHabich <orin.habich@student.uva.nl>
2017-11-24 11:12:06 +0100

    wat wel een beetje werkt

--- Homework/Week_4/test.js
+++ Homework/Week_4/test.js
@@ -1,37 +1,55 @@
+/*  test.js
+    Minor programmeren; DataProcessing; test SVG
+    Orin Habich 10689508
+*/
+
 d3.xml("test.svg", "image/svg+xml", function(error, xml) {
     if (error) throw error;
     document.body.appendChild(xml.documentElement);
     var colors = ["#ccece6","#99d8c9","#66c2a4","#41ae76","#238b45","#005824", "#D3D3D3"];
-    // // var x = d3.select("#kleur1");
-    // // console.log(x);
-    // // x.style('fill', colors[0]);
-    // for (var i = 0; i < 3; i++){
-    //   var id = "#kleur" + (i + 1);
-    //   d3.select(id)
-    //     .style('fill', colors[i]);
-    //   }
-    // for (var i = 4; i < colors.length; i++){
-    //   var id = "#kleur" + (i + 1);
-    //   d3.append("rect")
-    //     .attr("class", "st1")
-    //     .attr("id", id)
-    //     .attr("x", "13")
-    //     .attr("y", "140")
-    //     .attr("height", "29")
-    //     .attr("width", "21")
-    //     .style('fill', colors[i]);
-    // var colors = d3.scale.ordinal()
-    // .domain(d3.extent(dataset, function (d) { return d.colors; }))
-    // .range(["#ccece6","#99d8c9","#66c2a4","#41ae76","#238b45","#005824", "#D3D3D3"]);
-    //
+    var data = [1, 2, 3, 4, 5, 6, 7];
+    for (var i = 0; i < 3; i++){
+      var id = "#kleur" + (i + 1);
+      d3.select(id)
+        .style('fill', colors[i]);
+    }
 
-    d3.selectAll("rect")
-    .data(colors).enter()
-    .append("rect")
-    .attr("fill", function (colors){ return colors; })
-    .attr("x", function (colors, index){ return (index * 12) + "px"; })
-    .attr("y", 8 + "px")
-    .attr("width", 8 + "px")
-    .attr("height", 8 + "px");
+    for (var i = 4; i < colors.length; i++){
+    var id = "#kleur" + (i + 1);
+    d3.selectAll("svg")
+      .append("rect")
+      .attr("id", id)
+      .attr("x", "13")
+      .attr("y", (i - 1) * 43.4 + 2.8)
+      .attr("height", "29")
+      .attr("width", "21")
+      .style('fill', colors[i]);
 
+    if (i > 4){
+      var id = "#tekst" + (i + 1);
+      d3.selectAll("svg")
+        .append("rect")
+        .attr("id", id)
+        .attr("x", "46.5")
+        .attr("y", (i - 1) * 43.4 + 2.8)
+        .attr("height", "29")
+        .attr("width", "119.1")
+        .style('fill', "lightgrey");
+      }
+
+    var textblock = d3.selectAll("g")
+    .data(colors)
+    .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(46.5," + 200 + ")"; });
+
+textblock.append("rect")
+    .attr("width", 119.1)
+    .attr("height", 28);
+
+textblock.append("text")
+    .attr("x", 121)
+    .attr("y", 15)
+    .attr("dy", ".35em")
+    .text("tekst");
+    }
 });
