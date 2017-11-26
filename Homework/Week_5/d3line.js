c0ee170d0e0e49c9e4b13e00501ad679b5f98896
OrinHabich <orin.habich@student.uva.nl>
2017-11-26 16:11:56 +0100

    setup files

---
+++ Homework/Week_5/d3line.js
@@ -0,0 +1,10 @@
+/*  d3line.js
+    Minor programmeren; DataProcessing; D3 Line
+    Orin Habich 10689508
+
+    Here D3 is used to implement an interactive multiseries line graph.
+
+    Links to used sources:
+    d3js.org/d3.v3.min.js
+
+*/
