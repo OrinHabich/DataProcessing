0d96c30f02d824df6fcef1fde1c995a977606ab9
OrinHabich <orin.habich@student.uva.nl>
2017-11-26 16:30:14 +0100

    loading data

--- Homework/Week_5/d3line.js
+++ Homework/Week_5/d3line.js
@@ -8,3 +8,15 @@
     d3js.org/d3.v3.min.js
 
 */
+
+/*
+Make sure that you properly label the axes,
+ that the plot has a title and that the data source is explained.
+  Make sure that the date axis uses JavaScript dates,
+   something that is fairly simple using d3.time.format function.
+*/
+
+d3.json("RainJune2016BiltEindhoven.json", function(error, data) {
+  if (error) throw error
+  console.log(data)
+});
